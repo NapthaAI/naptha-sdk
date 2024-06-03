615f24746af64b693d8626431ba4287057429cdc
Richard Blythman <richardblythman@gmail.com>
2024-06-03 20:41:35 +0100

    move node schemas to sdk

--- naptha_sdk/schemas.py
+++ naptha_sdk/schemas.py
@@ -1,12 +1,39 @@
 from enum import Enum
 from typing import Dict, List, Optional
-from datetime import datetime, timedelta
+from datetime import datetime
 from pydantic import BaseModel
 
 class ModuleType(str, Enum):
     docker = "docker"
     template = "template"
     flow = "flow"
+
+class DockerJob(BaseModel):
+    docker_image: str
+    docker_command: Optional[str] = ""
+    docker_num_gpus: Optional[int] = 0
+    docker_env_vars: Optional[Dict] = None
+    input_dir: Optional[str] = None
+    input_ipfs_hash: Optional[str] = None
+    docker_input_dir: Optional[str] = None
+    docker_output_dir: Optional[str] = None
+    save_location: Optional[str] = None
+
+    class Config:
+        allow_mutation = True
+
+    class Config:
+        allow_mutation = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat(),
+        }
+
+    def model_dict(self):
+        model_dict = self.dict()
+        for key, value in model_dict.items():
+            if isinstance(value, datetime):
+                model_dict[key] = value.isoformat()
+        return model_dict
 
 class ModuleRun(BaseModel):
     module_name: str
@@ -54,5 +81,14 @@ class ModuleRunInput(BaseModel):
     consumer_id: str
     worker_nodes: Optional[list[str]] = None
     module_params: Optional[Dict] = None
+    docker_params: Optional[DockerJob] = None
     module_type: Optional[ModuleType] = None
     parent_runs: List['ModuleRun'] = []
+
+    def model_dict(self):
+        model_dict = self.dict()
+        for i, parent_run in enumerate(model_dict['parent_runs']):
+            for key, value in parent_run.items():
+                if isinstance(value, datetime):
+                    model_dict['parent_runs'][i][key] = value.isoformat()
+        return model_dict
