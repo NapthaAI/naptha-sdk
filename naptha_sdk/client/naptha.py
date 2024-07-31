72e760c24fcad9242ed7651d32ab9eccd33e7e1a
Richard Blythman <richardblythman@gmail.com>
2024-07-31 19:54:53 +0100

    Task receives Naptha client as arg

--- naptha_sdk/client/naptha.py
+++ naptha_sdk/client/naptha.py
@@ -1,4 +1,4 @@
-
+from huggingface_hub import HfApi, login
 from naptha_sdk.client.hub import Hub
 from naptha_sdk.client.node import Node
 from naptha_sdk.client.services import Services
@@ -11,16 +11,21 @@ class Naptha:
             user,
             hub_username, 
             hub_password, 
-            hub_url,
-            node_url,
-            routing_url,
-            indirect_node_id,
+            hf_username,
+            hf_access_token,
+            hub_url="ws://node.naptha.ai:3001/rpc",
+            node_url="http://node.naptha.ai:7001",
+            routing_url=None,
+            indirect_node_id=None,
             *args, 
             **kwargs
     ):
         
         self.user = user
         self.hub_url = hub_url
+        self.hf_username = hf_username
+        login(hf_access_token)
+        self.hf = HfApi()
         self.node_url = node_url
         self.routing_url = routing_url
         self.indirect_node_id = indirect_node_id
