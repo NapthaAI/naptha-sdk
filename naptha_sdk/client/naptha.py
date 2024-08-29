88c8c29c6691429d9dde2f53c86c1e0f04f07a67
Richard Blythman <richardblythman@gmail.com>
2024-08-29 13:16:54 +0100

    use async parent class for naptha client

--- naptha_sdk/client/naptha.py
+++ naptha_sdk/client/naptha.py
@@ -1,9 +1,10 @@
 from huggingface_hub import HfApi, login
 from naptha_sdk.client.hub import Hub
 from naptha_sdk.client.node import Node
+from naptha_sdk.utils import AsyncMixin
 from typing import Dict, List, Tuple
 
-class Naptha:
+class Naptha(AsyncMixin):
     """The entry point into Naptha."""
 
     def __init__(self,
@@ -33,8 +34,7 @@ class Naptha:
             routing_url=routing_url,
             indirect_node_id=indirect_node_id
         )
-        self.__storedargs = user, hub_username, hub_password, hub_url, node_url, args, kwargs
-        self.async_initialized = False
+        super().__init__()
 
     async def __ainit__(self,
             user,
@@ -48,21 +48,3 @@ class Naptha:
             **kwargs):
         """Async constructor"""
         self.hub = await Hub(hub_username, hub_password, hub_url)
-
-    async def __initobj(self):
-        """Crutch used for __await__ after spawning"""
-        assert not self.async_initialized
-        self.async_initialized = True
-        await self.__ainit__(
-            self.__storedargs[0], 
-            self.__storedargs[1], 
-            self.__storedargs[2], 
-            self.__storedargs[3], 
-            self.__storedargs[4], 
-            self.__storedargs[5], 
-            self.__storedargs[6],
-        )
-        return self
-
-    def __await__(self):
-        return self.__initobj().__await__()
