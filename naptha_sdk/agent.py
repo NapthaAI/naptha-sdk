ffdc3f851e887883a32ae25e3307194a46b88971
Richard Blythman <richardblythman@gmail.com>
2024-11-07 18:27:54 +0000

    return results of agent call

--- naptha_sdk/agent.py
+++ naptha_sdk/agent.py
@@ -6,15 +6,21 @@ logger = get_logger(__name__)
 class Agent:
     def __init__(self, 
         name, 
-        module, 
+        fn, 
         worker_node_url,
-        user,
+        orchestrator_node, 
+        flow_run, 
+        cfg,
+        task_engine_cls,
+        node_cls,
     ):
         self.name = name
-        self.module = module
-        self.user = user
+        self.fn = fn
+        self.orchestrator_node = orchestrator_node
+        self.flow_run = flow_run
+        self.task_engine_cls = task_engine_cls
         if isinstance(worker_node_url, str):
-            self.worker_node = Node(worker_node_url)
+            self.worker_node = self.node_url_to_node(worker_node_url, node_cls)
         else:
             self.worker_node = worker_node_url
 
@@ -22,12 +28,22 @@ class Agent:
         logger.info(f"Running agent on worker node {self.worker_node.node_url}")
 
         agent_run_input = {
-            "consumer_id": self.user.id,
+            "consumer_id": self.flow_run.consumer_id,
             "worker_nodes": [self.worker_node.node_url],
-            "agent_name": self.module,
+            "agent_name": self.fn,
             "agent_run_type": "package",
             "agent_run_params": kwargs,
         }
 
         agent_run = await self.worker_node.run_agent(agent_run_input=agent_run_input)
+        return agent_run
     
+    def node_url_to_node(self, node_url, node_cls):
+        if 'ws://' in node_url:
+            return node_cls(node_url, 'ws')
+        elif 'http://' in node_url:
+            return node_cls(node_url, 'http')
+        elif '://' not in node_url:
+            return node_cls(node_url, 'grpc')
+        else:
+            raise ValueError(f"Invalid node URL: {node_url}")
