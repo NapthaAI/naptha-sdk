e428c09863fbba02bc567104929c554cdc167252
Richard Blythman <richardblythman@gmail.com>
2024-11-18 18:23:11 +0000

    update agent class

--- naptha_sdk/agent.py
+++ naptha_sdk/agent.py
@@ -1,50 +1,27 @@
 from naptha_sdk.client.node import Node
+from naptha_sdk.schemas import AgentRunInput
 from naptha_sdk.utils import get_logger
 
 logger = get_logger(__name__)
 
 class Agent:
     def __init__(self, 
-        name, 
-        fn, 
-        worker_node_url,
-        orchestrator_node, 
-        flow_run, 
-        cfg,
-        task_engine_cls,
-        node_cls,
-        **kwargs
+        orchestrator_run, 
+        agent_index,
     ):
-        self.name = name
-        self.fn = fn
-        self.orchestrator_node = orchestrator_node
-        self.flow_run = flow_run
-        self.task_engine_cls = task_engine_cls
-        if isinstance(worker_node_url, str):
-            self.worker_node = self.node_url_to_node(worker_node_url, node_cls)
-        else:
-            self.worker_node = worker_node_url
+        self.orchestrator_run = orchestrator_run
+        self.agent_index = agent_index
+        worker_node_url = self.orchestrator_run.agent_deployments[self.agent_index].worker_node_url
+        self.worker_node = Node(worker_node_url)
 
     async def call_agent_func(self, *args, **kwargs):
         logger.info(f"Running agent on worker node {self.worker_node.node_url}")
 
-        agent_run_input = {
-            "consumer_id": self.flow_run.consumer_id,
-            "worker_nodes": [self.worker_node.node_url],
-            "agent_name": self.fn,
-            "agent_run_type": "package",
-            "agent_run_params": kwargs,
-        }
+        agent_run_input = AgentRunInput(
+            consumer_id=self.orchestrator_run.consumer_id,
+            inputs=kwargs,
+            agent_deployment=self.orchestrator_run.agent_deployments[self.agent_index].model_dump(),
+        )
 
-        agent_run = await self.worker_node.run_agent(agent_run_input=agent_run_input)
+        agent_run = await self.worker_node.run_agent_and_poll(agent_run_input=agent_run_input)
         return agent_run
-    
-    def node_url_to_node(self, node_url, node_cls):
-        if 'ws://' in node_url:
-            return node_cls(node_url, 'ws')
-        elif 'http://' in node_url:
-            return node_cls(node_url, 'http')
-        elif '://' not in node_url:
-            return node_cls(node_url, 'grpc')
-        else:
-            raise ValueError(f"Invalid node URL: {node_url}")
