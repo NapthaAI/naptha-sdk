d23381acac1b2109c08e2e7ccbcafb6cbc4b84fd
kamathis4 <kamathis4@gmail.com>
2024-12-20 11:28:20 +0530

    fix the import grpc pb

--- naptha_sdk/client/grpc_server_pb2_grpc.py
+++ naptha_sdk/client/grpc_server_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-from node.client import grpc_server_pb2 as grpc__server__pb2
+from naptha_sdk.client import grpc_server_pb2
 
 GRPC_GENERATED_VERSION = '1.68.0'
 GRPC_VERSION = grpc.__version__
