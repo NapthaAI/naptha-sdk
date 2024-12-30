927904e6c8c09aa573648d155f8298ed1addf8fe
Mohamed Arshath <marshath@gmail.com>
2024-12-30 01:06:02 +0000

    agent use NodeClient

--- naptha_sdk/modules/agent.py
+++ naptha_sdk/modules/agent.py
@@ -1,6 +1,6 @@
-from naptha_sdk.client.node import Node
+from naptha_sdk.client.node import NodeClient
 from naptha_sdk.schemas import AgentRunInput
-from naptha_sdk.utils import get_logger, node_to_url
+from naptha_sdk.utils import get_logger
 
 logger = get_logger(__name__)
 
@@ -13,7 +13,7 @@ class Agent:
     ):
         self.module_run = module_run
         self.agent_index = agent_index
-        self.worker_node = Node(self.module_run.deployment.agent_deployments[self.agent_index].node)
+        self.worker_node = NodeClient(self.module_run.deployment.agent_deployments[self.agent_index].node)
 
     async def call_agent_func(self, *args, **kwargs):
         logger.info(f"Running agent on worker node {self.worker_node.node_url}")
@@ -24,5 +24,5 @@ class Agent:
             deployment=self.module_run.deployment.agent_deployments[self.agent_index].model_dump(),
         )
         
-        agent_run = await self.worker_node.run_agent_in_node(agent_run_input)
+        agent_run = await self.worker_node.run_agent(agent_run_input)
         return agent_run
