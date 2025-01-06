8b11864a668d6eb282ee1aaa341a0da710e53f0f
Richard Blythman <richardblythman@gmail.com>
2025-01-06 12:21:36 +0000

    add check register user to setup deployment

--- naptha_sdk/configs.py
+++ naptha_sdk/configs.py
@@ -2,8 +2,9 @@ import json
 import os
 from pathlib import Path
 from naptha_sdk.client.hub import list_nodes
+from naptha_sdk.client.node import UserClient
 from naptha_sdk.module_manager import load_persona
-from naptha_sdk.schemas import AgentDeployment, EnvironmentDeployment, LLMConfig, OrchestratorDeployment, ToolDeployment, KBDeployment, KBConfig, AgentConfig, EnvironmentConfig, ToolConfig, OrchestratorConfig
+from naptha_sdk.schemas import AgentDeployment, EnvironmentDeployment, LLMConfig, OrchestratorDeployment, ToolDeployment, KBDeployment, KBConfig, AgentConfig, EnvironmentConfig, ToolConfig, OrchestratorConfig, NodeConfig
 from naptha_sdk.utils import url_to_node
 
 def load_llm_configs(llm_configs_path):
@@ -22,8 +23,20 @@ async def load_node_metadata(deployment, node_url, is_subdeployment):
         deployment["node"] = url_to_node(node_url)
     else:
         deployment["node"] = await list_nodes(deployment["node"]["ip"])
+        deployment["node"] = NodeConfig(**deployment["node"])
     print(f"Node metadata loaded {deployment['node']}")
     return deployment
+
+async def check_register_user(deployment, user_id=None):
+    node = UserClient(deployment["node"])
+    user = await node.check_user(user_input={"public_key": user_id.split(":")[-1]})
+
+    if user['is_registered'] == True:
+        print("Found user...", user)
+    else:
+        print("No user found. Registering user...")
+        user = await node.register_user(user_input=user)
+        print(f"User registered: {user}.")
 
 async def load_module_config_data(module_type, deployment, load_persona_data=False):
 
@@ -49,7 +62,7 @@ async def load_module_config_data(module_type, deployment, load_persona_data=Fal
 
     return deployment
 
-async def load_subdeployments(deployment, node_url):
+async def load_subdeployments(deployment, node_url, user_id=None):
 
     configs_path = Path(f"{Path.cwd().name}/configs")
 
@@ -58,34 +71,34 @@ async def load_subdeployments(deployment, node_url):
         agent_deployments = []
         for i, agent_deployment in enumerate(deployment["agent_deployments"]):
             deployment_name = deployment["agent_deployments"][i]["name"]
-            agent_deployment = await setup_module_deployment("agent", configs_path / "agent_deployments.json", node_url, deployment_name, is_subdeployment=True)
+            agent_deployment = await setup_module_deployment("agent", configs_path / "agent_deployments.json", node_url, user_id, deployment_name, is_subdeployment=True)
             agent_deployments.append(agent_deployment)
         deployment["agent_deployments"] = agent_deployments
     if "tool_deployments" in deployment and deployment["tool_deployments"]:
         tool_deployments = []
         for i, tool_deployment in enumerate(deployment["tool_deployments"]):
             deployment_name = deployment["tool_deployments"][i]["name"]
-            tool_deployment = await setup_module_deployment("tool", configs_path / "tool_deployments.json", node_url, deployment_name, is_subdeployment=True)
+            tool_deployment = await setup_module_deployment("tool", configs_path / "tool_deployments.json", node_url, user_id, deployment_name, is_subdeployment=True)
             tool_deployments.append(tool_deployment)
         deployment["tool_deployments"] = tool_deployments
     if "environment_deployments" in deployment and deployment["environment_deployments"]:
         environment_deployments = []
         for i, environment_deployment in enumerate(deployment["environment_deployments"]):
             deployment_name = deployment["environment_deployments"][i]["name"]
-            environment_deployment = await setup_module_deployment("environment", configs_path / "environment_deployments.json", node_url, deployment_name, is_subdeployment=True)
+            environment_deployment = await setup_module_deployment("environment", configs_path / "environment_deployments.json", node_url, user_id, deployment_name, is_subdeployment=True)
             environment_deployments.append(environment_deployment)
         deployment["environment_deployments"] = environment_deployments
     if "kb_deployments" in deployment and deployment["kb_deployments"]:
         kb_deployments = []
         for i, kb_deployment in enumerate(deployment["kb_deployments"]):
             deployment_name = deployment["kb_deployments"][i]["name"]
-            kb_deployment = await setup_module_deployment("kb", configs_path / "kb_deployments.json", node_url, deployment_name, is_subdeployment=True)
+            kb_deployment = await setup_module_deployment("kb", configs_path / "kb_deployments.json", node_url, user_id, deployment_name, is_subdeployment=True)
             kb_deployments.append(kb_deployment)
         deployment["kb_deployments"] = kb_deployments
     print(f"Subdeployments loaded {deployment}")
     return deployment
 
-async def setup_module_deployment(module_type: str, deployment_path: str, node_url: str, deployment_name: str = None, load_persona_data=False, is_subdeployment: bool = False):
+async def setup_module_deployment(module_type: str, deployment_path: str, node_url: str, user_id: str = None, deployment_name: str = None, load_persona_data=False, is_subdeployment: bool = False):
 
     # Map deployment types to their corresponding classes
     deployment_map = {
@@ -109,6 +122,8 @@ async def setup_module_deployment(module_type: str, deployment_path: str, node_u
             raise ValueError(f"No deployment found with name {deployment_name}")
 
     deployment = await load_node_metadata(deployment, node_url, is_subdeployment)
+    if user_id is not None:
+        await check_register_user(deployment, user_id)
     deployment = await load_module_config_data(module_type, deployment, load_persona_data)
-    deployment = await load_subdeployments(deployment, node_url)
+    deployment = await load_subdeployments(deployment, node_url, user_id)
     return deployment_map[module_type](**deployment)
