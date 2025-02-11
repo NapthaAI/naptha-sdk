bc7ad0f50f79e6fd726fbeeae6d3cc07fe59197f
Mohamed Arshath <marshath@gmail.com>
2025-02-11 02:29:53 +0000

    Add generic module creation method to UserClient and update Agent class

--- naptha_sdk/modules/agent.py
+++ naptha_sdk/modules/agent.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-from naptha_sdk.client.node import NodeClient
-from naptha_sdk.schemas import AgentRunInput
+from naptha_sdk.client.node import NodeClient, UserClient
+from naptha_sdk.schemas import AgentRunInput, AgentDeployment
 from naptha_sdk.utils import get_logger
 
 logger = get_logger(__name__)
@@ -8,15 +8,22 @@ load_dotenv(override=True)
 
 class Agent:
     def __init__(self, 
-        deployment, 
+        deployment=None, 
+        node_config=None,
         *args,
         **kwargs
     ):
-        self.deployment = deployment
-        self.agent_node = NodeClient(self.deployment.node)
+        if deployment:
+            self.deployment = deployment
+            self.agent_node = UserClient(deployment.node)
+
+        if node_config:
+            self.agent_node = UserClient(node_config)
 
     async def call_agent_func(self, module_run_input: AgentRunInput, *args, **kwargs):
         logger.info(f"Running agent on worker node {self.agent_node.node_url}")
-        
         agent_run = await self.agent_node.run_module(module_type="agent", run_input=module_run_input)
         return agent_run
+
+    async def create(self, module_request: AgentDeployment):
+        return await self.agent_node.create(module_type="agent", module_request=module_request)
