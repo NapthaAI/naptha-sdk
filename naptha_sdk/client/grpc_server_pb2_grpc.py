9c1364f60a41ec0f6ecb0992235b6ed980a635ac
Mohamed Arshath <marshath@gmail.com>
2025-02-19 02:56:41 +0000

    chore: Update gRPC generated code to compatible versions

--- naptha_sdk/client/grpc_server_pb2_grpc.py
+++ naptha_sdk/client/grpc_server_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from naptha_sdk.client import grpc_server_pb2 as grpc__server__pb2
 
-GRPC_GENERATED_VERSION = '1.68.1'
+GRPC_GENERATED_VERSION = '1.66.2'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
